aafd72dfb2d6d247c71e892e4a7fdd9cdd1bbae0
anushkasharma <aanushka.sharma@mail.utoronto.ca>
2025-10-13 12:00:27 -0400

    uncomment other apis

--- backend/app/main.py
+++ backend/app/main.py
@@ -13,27 +13,24 @@ from database.users_repository import UsersRepository
 
 UPLOAD_FOLDER = "backend/app/storage/uploads"
 
-
 app = Flask(__name__)
 app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
 
+@app.route("/extract/events", methods=["POST"])
+def extract_events():
+    filepath, error_response = handle_file_upload(request, app.config["UPLOAD_FOLDER"])
+    if error_response:
+        return error_response
+    result = extract_events_from_pdf(filepath)
+    return result, 200, {"Content-Type": "application/json"}
 
-
-# @app.route("/extract/events", methods=["POST"])
-# def extract_events():
-#     filepath, error_response = handle_file_upload(request, app.config["UPLOAD_FOLDER"])
-#     if error_response:
-#         return error_response
-#     result = extract_events_from_pdf(filepath)
-#     return result, 200, {"Content-Type": "application/json"}
-
-# @app.route("/extract/tasks", methods=["POST"])
-# def extract_tasks():
-#     filepath, error_response = handle_file_upload(request, app.config["UPLOAD_FOLDER"])
-#     if error_response:
-#         return error_response
-#     result = extract_tasks_from_pdf(filepath)
-#     return result, 200, {"Content-Type": "application/json"}
+@app.route("/extract/tasks", methods=["POST"])
+def extract_tasks():
+    filepath, error_response = handle_file_upload(request, app.config["UPLOAD_FOLDER"])
+    if error_response:
+        return error_response
+    result = extract_tasks_from_pdf(filepath)
+    return result, 200, {"Content-Type": "application/json"}
 
 # ---------- DB ROUTES ----------
 @app.route("/db/users", methods=["GET"])
