e29fb3cb1eebbe6b4986fe351cfc2810ad06bba3
xiezoey <zoey.xie@mail.utoronto.ca>
2025-10-14 14:06:33 -0400

    task -> assignments

--- frontend/src/components/course-container/index.tsx
+++ frontend/src/components/course-container/index.tsx
@@ -5,20 +5,20 @@ function CourseContainer ({ name, courseId, colour }: { name: string, courseId: 
 
     // grab tasks from API based on course name
     // for now, use dummy data
-    const [tasks, setTasks] = useState([
+    const [assignList, setAssignList] = useState([
         { id: 1, title: 'longlonglonglonglonglonglonglonglonglonglong', dueDate: '2023-10-01', course: name },
-        { id: 2, title: 'Task 2', dueDate: '2023-10-02', course: name },
-        { id: 3, title: 'Task 3', dueDate: '2023-10-03', course: name }
+        { id: 2, title: 'Assignment 2', dueDate: '2023-10-02', course: name },
+        { id: 3, title: 'Assignment 3', dueDate: '2023-10-03', course: name }
     ]);
-    
+
     return (
-        <div className={`flex flex-col rounded-lg bg-gradient-to-bl from-${colour}-100 to-${colour}-200 p-3 pt-5` }>
+        <div className={`flex flex-col rounded-lg bg-gradient-to-bl from-${colour}-100 to-${colour}-200 p-3 pt-5`}>
             <div className="mb-4">
-                <p className={`rounded-full text-sm text-center w-24 text-white bg-${colour}-400 px-2`}>{name}</p>
+                <span className={`inline-block rounded-full text-sm font-medium text-white bg-${colour}-400 py-1 px-3 truncate max-w-full`}>{name}</span>
             </div>
             <div>
                 <div className="w-full">
-                    <WideContainer tasks={tasks} colour={colour} />
+                    <WideContainer tasks={assignList} colour={colour} />
                 </div>
             </div>
         </div>
