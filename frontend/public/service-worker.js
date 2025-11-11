215abb883cc0f59a15e6460bd7c0c17814ed4531
xiezoey <zoey.xie@mail.utoronto.ca>
2025-11-11 16:53:27 -0500

    demo notification

--- frontend/public/service-worker.js
+++ frontend/public/service-worker.js
@@ -8,6 +8,10 @@ chrome.action.onClicked.addListener(async (tab) => {
   try {
     await chrome.sidePanel.open({ windowId: tab.windowId });
     console.log('Side panel opened');
+
+    // example notification that pops up after 1 minute of opening the side panel
+    chrome.alarms.create({ delayInMinutes: 2 });
+
   } catch (error) {
     console.error('Error opening side panel:', error);
   }
@@ -16,4 +20,15 @@ chrome.action.onClicked.addListener(async (tab) => {
 // Optional: Handle side panel setup
 chrome.runtime.onStartup.addListener(() => {
   console.log('Extension started');
+});
+
+chrome.alarms.onAlarm.addListener(() => {
+  chrome.action.setBadgeText({ text: '' });
+  chrome.notifications.create({
+    type: 'basic',
+    iconUrl: 'achievo-clap-transparent.png',
+    title: 'Achievo Reminder',
+    message: 'Time to check your Achievo tasks!',
+    priority: 2,
+  });
 });
